3ba3670b5c707bc1f21eafc53b54aa9b92ee7a9d
Tathagat27 <tat27302@gmail.com>
2023-07-09 17:03:49 +0530

    Cart page added

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
   Route,
   Link,
 } from "react-router-dom";
+import CartPage from "./pages/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/signup",
     element: <SignupPage />,
+  },
+  {
+    path: "/cart",
+    element: <CartPage />,
   },
 ]);
 
