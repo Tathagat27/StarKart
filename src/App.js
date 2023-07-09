ca460528bdecfc0ade304194c292ebba9b7d7ac6
Tathagat27 <tat27302@gmail.com>
2023-07-09 23:17:32 +0530

    Checkout and productDetail pages has been added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import {
   Link,
 } from "react-router-dom";
 import CartPage from "./pages/CartPage";
+import Checkout from "./pages/Checkout";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const router = createBrowserRouter([
   {
@@ -30,6 +32,14 @@ const router = createBrowserRouter([
   {
     path: "/cart",
     element: <CartPage />,
+  },
+  {
+    path: "/checkout",
+    element: <Checkout />,
+  },
+  {
+    path: "/product-detail",
+    element: <ProductDetailPage />,
   },
 ]);
 
